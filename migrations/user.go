e37c87bf8e862ae698017d3e232371914d207a8d
Azamat31415 <230147@astanait.edu.kz>
2025-01-25 19:38:39 +0500

    Fix

--- migrations/user.go
+++ migrations/user.go
@@ -1,6 +1,7 @@
 package migrations
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -13,6 +14,32 @@ type User struct {
 	LastName  string
 	Address   string
 	Phone     string
+}
+
+// UserRegistration for data structure of registration
+type UserRegistration struct {
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Address   string `json:"address"`
+	Phone     string `json:"phone"`
+}
+
+// HashPassword for hash a password
+func (user *User) HashPassword() error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hash)
+	return nil
+}
+
+// CheckPassword for checking a password
+func (user *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	return err == nil
 }
 
 // MigrateUser for creation of user table
