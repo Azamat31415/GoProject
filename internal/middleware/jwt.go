f7f9433c403cf4e3e7045b232fbca2c611a17f1f
Azamat31415 <230147@astanait.edu.kz>
2025-01-28 22:33:49 +0500

    I made a mistake

--- internal/middleware/jwt.go
+++ internal/middleware/jwt.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"GoProject/handlers/auth_handlers"
-	"context"
 	"fmt"
 	"net/http"
 	"strings"
@@ -11,10 +9,6 @@ import (
 )
 
 var jwtSecret = []byte("9Q7MvM4M7jpDq6fjk8jMKVuY=n8RygTXGphFcz3L7txy")
-
-type ContextKey string
-
-const UserIDKey ContextKey = "user_id"
 
 func JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -25,12 +19,6 @@ func JWTMiddleware(next http.Handler) http.Handler {
 		}
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-
-		if auth_handlers.IsTokenRevoked(tokenString) {
-			http.Error(w, "Token has been revoked", http.StatusUnauthorized)
-			return
-		}
-
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			if token.Method != jwt.SigningMethodHS256 {
 				return nil, fmt.Errorf("unexpected signing method: %v", token.Method.Alg())
@@ -43,19 +31,6 @@ func JWTMiddleware(next http.Handler) http.Handler {
 			return
 		}
 
-		claims, ok := token.Claims.(jwt.MapClaims)
-		if !ok {
-			http.Error(w, "Invalid token claims", http.StatusUnauthorized)
-			return
-		}
-
-		userID, ok := claims["user_id"].(float64)
-		if !ok {
-			http.Error(w, "Invalid user ID in token", http.StatusUnauthorized)
-			return
-		}
-
-		ctx := context.WithValue(r.Context(), UserIDKey, uint(userID))
-		next.ServeHTTP(w, r.WithContext(ctx))
+		next.ServeHTTP(w, r)
 	})
 }
